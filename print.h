9d46da09b6c217f8e03bb84fea33f07c881f6259
Beta-Cygni-A <theinnovativeadventurer@gmail.com>
2024-01-03 21:46:48 -0700

    update print.h with comments

--- print.h
+++ print.h
@@ -1,5 +1,5 @@
-#ifndef HEADONLY_H
-#define HEADONLY_H
+#ifndef PRINT_H
+#define PRINT_H
 
 #include <iostream>
 #include <string_view>
@@ -27,22 +27,25 @@ public:
 		((std::cout << [&args]() { return ((args == "nLN") ? "\n" : (args == "nSP") ? " " : args);}() << ""), ...);
 	}
 
+//End of Class
+
 namespace mySTD // for implementing features wanted and knockoffs of features currently unavailable to me
 {
 	template <typename... Q1>
 
-	inline void printAln(const Q1&... args)
+	inline void printAln(const Q1&... args) //takes any number of arguments and puts a space after eachone
 	{
 	        ((std::cout << args << " "), ...);
 	}
 
 	template <typename... Q1>
 
-	inline void printNln(const Q1&... args)
+	inline void printNln(const Q1&... args) //takes any number of arguments and puts a newline after eachone
 	{
 	        ((std::cout << args << "\n"), ...);
 	}
 
+//Old Function
 /*
 	inline auto checkArgs(const auto& args)
 	{
@@ -51,10 +54,11 @@ namespace mySTD // for implementing features wanted and knockoffs of features cu
 */
 	template <typename... Q1>
 
-	inline void prinT(const Q1&... args)
+	inline void prinT(const Q1&... args) //takes any number of arguments. Pass "" for a space, "\n" newline, or nospace by passing neither.
 	{	        
 		((std::cout << [&args]() { return ((args == "\n") ? "\n" : (args == "") ? " " : args);}() << ""), ...);
 
+// Old function call		
 //		((std::cout << checkArgs(args) << ""), ...);
 	}
 };
